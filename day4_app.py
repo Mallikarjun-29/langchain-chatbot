d1c5a6091f0984dbe2bd38826cf1e0a20154fd7d
Mallikharjun Mallela <mallikharjunmallela@Mallikharjuns-MacBook-Air.local>
2026-05-20 17:47:13 -0500

    Fix ChatPromptTemplate import

--- day4_app.py
+++ day4_app.py
@@ -7,7 +7,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
